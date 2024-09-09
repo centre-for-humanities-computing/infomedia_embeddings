bd1026878f178f59356602a7d7caf2d924d3d940
MinaAlmasi <mina.almasi00@gmail.com>
2024-09-09 15:21:04 +0200

    refactor: rm logfile + datetime, add argparse for save_path (make arg required, but give no default to avoid accidentally overwriting)

--- src/hpv_query.py
+++ src/hpv_query.py
@@ -1,14 +1,15 @@
 """
 Make a query for HPV articles in the corpus (based on original paper https://bmcpublichealth.biomedcentral.com/articles/10.1186/s12889-018-6268-x)
 
-Run the script to query corpus and save the results in a jsonl file (in dat/hpv_data/files_{date}).
+Run the script to query corpus and save the results in a jsonl file to specified path. Note that if you re-run the script with the same save path, the file will be overwritten!
+    python hpv_query.py --save_path <path_to_save> 
 
-Note that if you re-run the script in the same day, the script will overwrite existing files:
-    python hpv_query.py 
+Example: (where data is currently saved)
+    python hpv_query.py --save_path ../../infomedia-embedding/dat/hpv_data/
 """
+import argparse
 import json
 import pathlib
-from datetime import datetime
 from typing import Iterable
 
 from tqdm import tqdm
@@ -83,63 +84,55 @@ def process_one_file(file_path: str, text_key: str = "content") -> Iterable[dict
 def process_all_files(
     file_paths: list,
     save_file: pathlib.Path,
-    log_file: pathlib.Path,
     text_key="content",
 ):
     """
-    Process all files with an option to overwrite existing results and automatically reprocess the last file in the log if it exists.
-
-    Reprocessing the last file ensures that no data is complete if the process was interrupted mid-file.
+    Process all files and save the results in a jsonl file.
 
     args:
         file_paths: list
         save_file: pathlib.Path
-        log_file: pathlib.Path
         text_key: str
     """
-    for file in [save_file, log_file]:
-        if (
-            file.exists()
-        ):  # since we are appending to the logfile + savefile, we want to remove it if we re-run the script (to avoid duplicates)
-            file.unlink()
+    if save_file.exists() and save_file.suffix == ".jsonl": 
+        save_file.unlink() # since we are appending to the savefile, we want to remove it if we re-run the script (to avoid duplicates)
 
     for file_path in tqdm(file_paths):
         queried_entries = process_one_file(file_path, text_key)
 
-        article_count = 0
         with open(save_file, "a") as f:
             for entry in queried_entries:
                 f.write(json.dumps(entry) + "\n")
-                article_count += 1
 
-        with open(log_file, "a") as f:
-            log_data = {"file_path": str(file_path), "hpv_article_count": article_count}
-            f.write(json.dumps(log_data) + "\n")
-
+def input_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--save_path",
+        type=str,
+        help="Path to save the hpv query data",
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = input_parse()
     path = pathlib.Path(__file__)
-    corpus_path = path.parents[3] / "infomedia-embedding" / "dat" / "corpus"
 
+    # get files from corpus
+    corpus_path = path.parents[3] / "infomedia-embedding" / "dat" / "corpus"
     file_paths = [file for file in corpus_path.iterdir() if file.suffix == ".jsonl"]
     file_paths = sorted(file_paths)
 
-    # name folder with date (to avoid overwriting if the script is unintentionally suddenly re-run at a later time)
-    now = datetime.now()
-    date = now.strftime("%Y_%m_%d")
-
-    save_path = (
-        path.parents[3] / "infomedia-embedding" / "dat" / "hpv_data" / f"files_{date}"
-    )
+    # create save path
+    save_path = pathlib.Path(args.save_path)
     save_path.mkdir(exist_ok=True, parents=True)
 
     process_all_files(
         file_paths=file_paths,
         save_file=save_path / "hpv_query_data.jsonl",
-        log_file=save_path / "hpv_query_log.jsonl",
-        text_key="content",
+        text_key="content"
     )
-
 
 if __name__ == "__main__":
     main()
