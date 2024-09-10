7d834600f6a9e24493df6922629c0cd52b4f2311
MinaAlmasi <mina.almasi00@gmail.com>
2024-09-10 10:16:10 +0200

    fix: add default paths but allow paths as CLI args, rm unused import

--- src/create_embeddings.py
+++ src/create_embeddings.py
@@ -1,7 +1,12 @@
+"""
+Run embeddings on data jsonl file and save the embeddings in a numpy file: 
+
+python src/create_embeddings.py --in_file /path/to/data.jsonl --out_file /path/to/embeddings.npy
+"""
 import argparse
 import json
 import pathlib
-from typing import List, Literal
+from typing import Literal
 
 import numpy as np
 from sentence_transformers import SentenceTransformer
@@ -43,33 +48,50 @@ class E5Model:
 def input_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "model_name",
+        "--model_name",
         type=str,
         help="Name of the model to use.",
         default="intfloat/multilingual-e5-large",
     )
+    parser.add_argument(
+        "--in_file",
+        type=str,
+        help="Path to file with data to encode"
+    )
+
+    parser.add_argument(
+        "--out_file",
+        type=str,
+        help="Path to save the embeddings"
+    )
+
     args = parser.parse_args()
     return args
 
-
 def main():
+    args = input_parse()
     path = pathlib.Path(__file__)
-    data_path = (
-        path.parents[3]
-        / "infomedia-embedding"
-        / "dat"
-        / "hpv_data"
-    )
 
-    with open(data_path / "hpv_query_data.jsonl", "r") as f:
-        hpv_data = [json.loads(line) for line in f]
+    default_paths = {
+        "in_file": path.parents[3] / "infomedia-embedding" / "dat" / "hpv_data" / "hpv_query_data.jsonl",
+        "out_file": path.parents[3] / "infomedia-embedding" / "dat" / "hpv_data" / "hpv_embeddings.npy"
+    }
 
-    texts = [row["content"] for row in hpv_data]
+    # grab data from default path if not provided 
+    in_file = pathlib.Path(args.in_file) if args.in_file is not None else default_paths["in_file"]
+    with open(in_file, "r") as f:
+        data = [json.loads(line) for line in f]
 
-    model = E5Model(model_name="intfloat/multilingual-e5-large")
-    embeddings = model.encode(sentences=texts, batch_size=32, query_type="passage")
+    # extract sents
+    sents = [row["content"] for row in data]
+
+    # encode
+    model = E5Model(model_name=args.model_name) 
+    embeddings = model.encode(sentences=sents, batch_size=32, query_type="passage", normalize=False)
     
-    np.save(data_path / "hpv_embeddings.npy", embeddings)
+    # save embeddings
+    out_file = pathlib.Path(args.out_file) if args.out_file is not None else default_paths["out_file"]
+    np.save(out_file, embeddings)
 
 
 if __name__ == "__main__":
