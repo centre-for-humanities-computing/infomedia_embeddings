aefc14e92d72c192c58ef7ff3b422830f03363dd
Márton Kardos <power.up1163@gmail.com>
2024-11-18 17:32:31 +0100

    Finished up novelty-resonance plot

--- src/novelty_resonance.py
+++ src/novelty_resonance.py
@@ -1,11 +1,12 @@
 import json
 from datetime import datetime
 from pathlib import Path
-from scipy.spatial.distance import cosine
-import plotly.graph_objects as go
 
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from scipy.spatial.distance import cosine
 
 
 def load_jsonl(path: Path | str) -> list[dict]:
@@ -17,14 +18,16 @@ def load_jsonl(path: Path | str) -> list[dict]:
                 entries.append(json.loads(line))
     return entries
 
+
 def novelty(a: np.ndarray, window_size: int, metric=cosine) -> np.ndarray:
     res = []
     for j in range(len(a)):
         if j < window_size:
             res.append(np.nan)
         else:
-            res.append(np.mean([metric(a[j], a[j-d]) for d in range(window_size)]))
+            res.append(np.mean([metric(a[j], a[j - d]) for d in range(window_size)]))
     return np.array(res)
+
 
 def transience(a: np.ndarray, window_size: int, metric=cosine) -> np.ndarray:
     res = []
@@ -32,14 +35,17 @@ def transience(a: np.ndarray, window_size: int, metric=cosine) -> np.ndarray:
         if j > (len(a) - window_size - 1):
             res.append(np.nan)
         else:
-            res.append(np.mean([metric(a[j], a[j+d]) for d in range(window_size)]))
+            res.append(np.mean([metric(a[j], a[j + d]) for d in range(window_size)]))
     return np.array(res)
+
 
 records = load_jsonl("dat/hpv_query_data.jsonl")
 
 data = pd.DataFrame.from_records(records)
 data["date"] = pd.to_datetime(data["date"])
-data["month"] = data["date"].map(lambda dt: datetime(year=dt.year, month=dt.month, day=1))
+data["month"] = data["date"].map(
+    lambda dt: datetime(year=dt.year, month=dt.month, day=1)
+)
 
 events = pd.DataFrame(
     {
@@ -68,20 +74,42 @@ events = pd.DataFrame(
     }
 )
 events["date"] = pd.to_datetime(events["date"])
-models = ["keynmf_20", "s3_20"]
+models = ["keynmf", "s3"]
+fig = make_subplots(rows=2, cols=1, subplot_titles=["Novelty", "Resonance"])
 for model in models:
-    topics = pd.read_csv(f"topics/{model}_doc_topic_matrix.csv", index_col=0)
+    topics = pd.read_csv(f"topics/{model}_20_doc_topic_matrix.csv", index_col=0)
     topics = topics.join(data[["month"]])
     topics = topics.groupby("month").mean()
     topics = topics.sort_index()
-    topic_novelty = novelty(topics.to_numpy(), window_size=10)
-    topic_transience = transience(topics.to_numpy(), window_size=10)
+    topic_novelty = novelty(topics.to_numpy(), window_size=20)
+    topic_transience = transience(topics.to_numpy(), window_size=20)
     topic_resonance = topic_novelty - topic_transience
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(name="novelty", y=topic_novelty, x=topics.index))
-    fig.add_trace(go.Scatter(name="resonance", y=topic_resonance, x=topics.index))
+    fig.add_trace(
+        go.Scatter(
+            name=model,
+            y=topic_novelty,
+            x=topics.index,
+            line=dict(color="red" if model == "keynmf" else "blue"),
+            legendgroup=model,
+        ),
+        col=1,
+        row=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            name=model,
+            y=topic_resonance,
+            x=topics.index,
+            line=dict(color="red" if model == "keynmf" else "blue"),
+            legendgroup=model,
+            showlegend=False,
+        ),
+        col=1,
+        row=2,
+    )
+for facet in range(2):
     for idx, row in events.iterrows():
-        fig = fig.add_vline(x=row["date"])
+        fig = fig.add_vline(x=row["date"], col=1, row=facet + 1)
         fig = fig.add_annotation(
             x=row["date"],
             y=np.random.normal(0.25, 0.05),
@@ -90,7 +118,8 @@ for model in models:
             textangle=-90,
             xshift=0,
             xanchor="right",
+            col=1,
+            row=facet + 1,
         )
-    fig.update_layout(template="plotly_white")
-    fig.show()
-
+fig = fig.update_layout(width=1400, height=800, template="plotly_white")
+fig.write_html("figures/novelty_resonance.html")
